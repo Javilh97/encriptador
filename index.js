18bada00155e07042727a9939bf072b4db1a9006
Javier <javier.lhernandez97@gmail.com>
2023-01-19 20:43:14 -0600

    Se agrego la logica de encriptado

--- index.js
+++ index.js
@@ -7,17 +7,57 @@ La letra "a" es convertida para "ai"
 La letra "o" es convertida para "ober"
 La letra "u" es convertida para "ufat"
 */
-document.getElementById("copy").style.display = "none";
 
 
-let texto = "escuela";
+document.getElementById("copy").style.visibility = "hidden";
 
-for (let i = 0; i <= texto.length; i++) {
-    if (texto[i] === "e" ) {
-        texto[i].replace("enter");
-        console.log(texto[i]);
+
+
+function encript() {
+    //const regex = /[aeiou]/g;
+    
+    let string = document.getElementById("textEncript").value.toLowerCase();
+    //let nuevaString = string.match(regex).length;
+    //console.log(nuevaString);
+
+    let nuevaString = string.replace(/e/img, "enter");
+    nuevaString = nuevaString.replace(/i/img, "imes");
+    nuevaString = nuevaString.replace(/a/img, "ai");
+    nuevaString = nuevaString.replace(/o/img, "ober");
+    nuevaString = nuevaString.replace(/u/img, "ufat");
+
+    document.getElementById("ocultar").innerHTML = nuevaString;    
+    
+
+
+    /* for (let i = 0; i <= string.length; i++) {
+            
+        console.log(string[i].replace('e','enter'));
     }
+
+    console.log(nuevaString); */
+    document.getElementById("copy").style.visibility = "";
 }
 
-console.log(texto);
-console.log("Hola mundo");
+function desencriptar() {
+    let string = document.getElementById("textEncript").value;
+    //let nuevaString = string.match(regex).length;
+    //console.log(nuevaString);
+
+    let nuevaString = string.replace(/enter/img, "e");
+    nuevaString = nuevaString.replace(/imes/img, "i");
+    nuevaString = nuevaString.replace(/ai/img, "a");
+    nuevaString = nuevaString.replace(/ober/img, "o");
+    nuevaString = nuevaString.replace(/ufat/img, "u");
+
+    document.getElementById("ocultar").innerHTML = nuevaString;    
+    
+}
+
+function copy() {
+    let copyText = document.querySelector("#ocultar");
+    copyText.select();
+    document.execCommand("copy");
+  }
+
+
